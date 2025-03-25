19c00a835e4d052e934557fbea93b47ade871984
AmrTamer23 <amrtamer23mj@gmail.com>
2025-03-25 22:54:25 +0200

    feat: wip home ui

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Header } from "@/components/Header";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative min-h-screen flex flex-col px-4">
+      <Header />
+      <main className="flex-1 ">{children}</main>
+      <footer className="w-full border-t py-6 md:py-0">
+        <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
+          <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+            Built with ❤️ by{" "}
+            <a
+              href="https://github.com/amrtamer23"
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline underline-offset-4"
+            >
+              @AmrTamer23
+            </a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
